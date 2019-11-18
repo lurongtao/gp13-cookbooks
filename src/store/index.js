0720f808ec8650517b15510cbdefa0cd41138873
lurongtao <lurongtao@126.com>
2019-11-18 10:35:45 +0800

    2019-11-18-am-1

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,17 @@ import {
   applyMiddleware
 } from 'redux'
 
+import { Map } from 'immutable'
+
 import thunk from 'redux-thunk'
 
 import reducer from './reducer'
 
+const initState = Map({})
+
 const store = createStore(
   reducer,
+  initState,
   applyMiddleware(thunk)
 )
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,4 +1,7 @@
-import { combineReducers } from 'redux'
+// import { combineReducers } from 'redux'
+import {
+  combineReducers
+} from 'redux-immutable'
 import { reducer as list } from 'pages/home/cookbook/'
 import { reducer as layout } from 'pages/home/layout/'
 
