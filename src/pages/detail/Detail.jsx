7c00db146c5273bf24c5a766a6ec7998b8768f4e
lurongtao <lurongtao@126.com>
2019-11-19 21:04:51 +0800

    2019-11-19-pm-2

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -1,46 +1,108 @@
-import React, { 
-  createContext,
-  createRef,
-  forwardRef,
-  useState,
-  useEffect,
-  useContext,
-  useReducer,
-  useCallback,
-  useMemo,
-  useRef,
-  useImperativeHandle
-} from "react"
+// import React, { 
+//   createContext,
+//   createRef,
+//   forwardRef,
+//   useState,
+//   useEffect,
+//   useContext,
+//   useReducer,
+//   useCallback,
+//   useMemo,
+//   useRef,
+//   useImperativeHandle,
+//   useLayoutEffect
+// } from "react"
 
-// const Input = (props, ref) => {
-//   var x = 0
-//   // const inputRef = useRef()
-//   // useImperativeHandle(ref, () => inputRef.current)
+// const Child = (props) => {
+//   console.log('child')
 //   return (
-//     <input type="text"/>
+//     <div>hello</div>
 //   )
 // }
-class Input extends React.Component {
-  x = 0
-  render() {
-    return (
-      <input type="text"/>
-    )
-  }
+
+// const App = () => {
+//   let [title, setTitle] = useState('hello')
+//   let [style, setStyle] = useState({fontSize: '100px'})
+
+//   const handleMyClick = useMemo(() => () => console.log(0), [])
+
+//   const MemoChild = useMemo(() => {
+//     return (
+//       <Child style={style} onMyClick={handleMyClick}></Child>
+//     )
+//   }, [style, handleMyClick])
+
+//   useEffect(() => {
+//     setTitle('world')
+//   }, [])
+
+//   return MemoChild
+// }
+
+// const App = () => {
+//   let ref = useRef()
+//   let [title, setTitle] = useState('hello')
+
+//   useLayoutEffect(() => {
+//     console.log(0)
+//     console.log(title)
+//     setTitle('world')
+//   }, [title])
+
+//   useEffect(() => {
+//     console.log(1)
+//     console.log(title)
+//   }, [title])
+
+//   return (
+//     <div ref={ref}>{title}</div>
+//   )
+// }
+
+// function useGretting(str, setTitle) {
+//   setTimeout(() => {
+//     setTitle(str)
+//   }, 2000)
+// }
+
+// const Child1 = () => {
+//   let [title, setTitle] = useState('hello')
+//   useGretting('world', setTitle)
+
+//   // useEffect(() => {
+//   //   setTimeout(() => {
+//   //     setTitle('world')
+//   //   }, 2000)
+//   // }, [])
+
+//   return (
+//     <div>{title}</div>
+//   )
+// }
+
+// const Child2 = () => {
+//   let [title, setTitle] = useState('hi')
+//   useGretting('oh', setTitle)
+//   // useEffect(() => {
+//   //   setTimeout(() => {
+//   //     setTitle('oh')
+//   //   }, 2000)
+//   // }, [])
+
+//   return (
+//     <div>{title}</div>
+//   )
+// }
+
+// const App = () => {
+//   return (
+//     <>
+//       <Child1></Child1>
+//       <Child2></Child2>
+//     </>
+//   )
+// }
+
+export default () => {
+  return null
 }
-
-// const ChildInput = forwardRef(Input)
-
-const App = () => {
-  const appRef = createRef()
-
-  useEffect(() => {
-    console.log(appRef.current.x)
-  })
-
-  return (
-    <Input ref={appRef}></Input>
-  )
-}
-
-export default App
