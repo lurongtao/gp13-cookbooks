49f56fde5fb9c4914eb5e26da63b7f9a5cf761ee
lurongtao <lurongtao@126.com>
2019-11-21 18:04:05 +0800

    2019-11-21-pm

--- src/index.js
+++ src/index.js
@@ -13,3 +13,12 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+window.wx.config({
+  debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+  appId: 'wxd901da01d13166a9', // 必填，公众号的唯一标识
+  timestamp: 457439223, // 必填，生成签名的时间戳
+  nonceStr: 'slfuqo4szjsfd', // 必填，生成签名的随机串
+  signature: 'sdfassdf',// 必填，签名
+  jsApiList: ['scanQRCode'] // 必填，需要使用的JS接口列表
+})
